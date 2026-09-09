a98bdd67b15d0193d22239cda109832db8f8955c
ohuminho <ohuminho@gmail.com>
2026-09-09 06:03:42 +0000

    Create MARKA mobile architecture foundation

---
+++ src/mobile/components/MobileContainer.tsx
@@ -0,0 +1,7 @@
+export default function MobileContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
---
+++ src/mobile/navigation/routes.ts
@@ -0,0 +1,6 @@
+export const mobileRoutes = {
+  home: "/",
+  marketplace: "/marketplace",
+  wallet: "/wallet",
+  profile: "/profile",
+};
