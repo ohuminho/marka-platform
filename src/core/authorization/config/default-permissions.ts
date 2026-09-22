647df0578f3f7d77845c082cb9babfd0ec5508a5
ohuminho <ohuminho@gmail.com>
2026-09-22 11:02:42 +0100

    Update default-permissions.ts

--- src/core/authorization/config/default-permissions.ts
+++ src/core/authorization/config/default-permissions.ts
@@ -25,6 +25,8 @@ export const DefaultPermissions: PermissionAction[] = [
   Permissions.WALLET_READ,
   Permissions.WALLET_MANAGE,
 
+  Permissions.SETTLEMENT_PAYOUT_EXECUTE,
+
   Permissions.ADMIN_ACCESS,
   Permissions.SYSTEM_ADMIN,
 ];
