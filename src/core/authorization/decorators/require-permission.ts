1c6cca5c319a7247e9b0c0dcdc31e89f2c848921
ohuminho <ohuminho@gmail.com>
2026-09-24 03:29:08 +0100

    Update require-permission.ts

--- src/core/authorization/decorators/require-permission.ts
+++ src/core/authorization/decorators/require-permission.ts
@@ -1,6 +1,16 @@
+export interface RequiredPermission {
+  permission: string;
+}
+
 export function RequirePermission(
   permission: string
-) {
+): RequiredPermission {
+  if (!permission.trim()) {
+    throw new Error(
+      "Permission is required."
+    );
+  }
+
   return {
     permission,
   };
