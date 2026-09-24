4d6c13116cdb281ff6ea831e3359e48b4bc2aeba
ohuminho <ohuminho@gmail.com>
2026-09-24 03:19:48 +0100

    Update password.service.ts

--- src/core/authentication/password.service.ts
+++ src/core/authentication/password.service.ts
@@ -1,20 +1,48 @@
 import bcrypt from "bcrypt";
 
+import { AuthConfig } from "./auth.config";
+
 export class PasswordService {
+  async hash(password: string): Promise<string> {
+    this.validatePassword(password);
 
-  async hash(password: string) {
-    return bcrypt.hash(password, 12);
+    return bcrypt.hash(
+      password,
+      AuthConfig.password.bcryptRounds
+    );
   }
-
 
   async compare(
     password: string,
     hash: string
-  ) {
-    return bcrypt.compare(
-      password,
-      hash
-    );
+  ): Promise<boolean> {
+    if (!password || !hash) {
+      return false;
+    }
+
+    return bcrypt.compare(password, hash);
   }
 
+  validatePassword(password: string): void {
+    if (
+      password.length <
+      AuthConfig.password.minimumLength
+    ) {
+      throw new Error(
+        `Password must contain at least ${AuthConfig.password.minimumLength} characters.`
+      );
+    }
+
+    if (
+      password.length >
+      AuthConfig.password.maximumLength
+    ) {
+      throw new Error(
+        `Password must not exceed ${AuthConfig.password.maximumLength} characters.`
+      );
+    }
+  }
 }
+
+export const passwordService =
+  new PasswordService();
